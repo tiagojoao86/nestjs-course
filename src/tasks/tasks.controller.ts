87ef983a8294f08144a80b57fd247553248b851d
Tiago Joao Pereira <pereira.tiagojoao@gmail.com>
2020-06-08 05:57:10 -0300

    08062020_casa

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -11,10 +12,22 @@ export class TasksController {
   public getAllTasks(): Task[] {
     return this.taskService.getTasks();
   }
+
+  @Get(':id')
+  public getTaskById(@Param('id') id: string): Task {
+    return this.taskService.getTaskById(id);
+  }
   
+  // @Post()
+  // public createTask(@Body() body) {
+  //   console.log('body', body);
+  //   return this.taskService.createTask(body.title, body.description);
+  // }
+
   @Post()
-  public createTask(@Body() body) {
-    console.log('body', body);
+  public createTask(@Body() createTaskDto: CreateTaskDto) {
+    console.log('body', createTaskDto);
+    return this.taskService.createTask(createTaskDto);
   }
 
   @Post('other')
@@ -24,7 +37,7 @@ export class TasksController {
 
     console.log('title: ' + title + ', description:' + description);
 
-    this.taskService.createTask(title, description);
+    return this.taskService.createTaskOther(title, description);
   }
 
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid/dist';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 
 @Injectable()
@@ -16,7 +17,26 @@ export class TasksService {
     return this.tasks;
   }
 
-  public createTask(title: string, description: string): Task {
+  public getTaskById(id: string): Task {
+    return this.tasks.find(task => task.id == id); 
+  }
+
+  public createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN
+    }
+
+    this.tasks.push(task);
+
+    return task;
+  }
+
+  public createTaskOther(title: string, description: string): Task {
     const task: Task = {
       id: uuid(),
       title,
