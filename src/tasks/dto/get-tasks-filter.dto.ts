4aada9da6c482a566ab6a43b3e582c458c0b54eb
Tiago Pereira <tiago.pereira@branetlogistica.com.br>
2020-06-15 13:31:08 -0300

    15062020_1331

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,8 @@
 import { TaskStatus } from "../task.model";
+import { IsNotEmpty } from "class-validator"
 
 export class GetTasksFilterDto {
+    
     status: TaskStatus;
     search: string;
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, Delete, Patch, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -32,6 +32,7 @@ export class TasksController {
   // }
 
   @Post()
+  @UsePipes(ValidationPipe)
   public createTask(@Body() createTaskDto: CreateTaskDto) {
     console.log('body', createTaskDto);
     return this.taskService.createTask(createTaskDto);
